5bac87426272d903cbb08c4d1243d90eb15e7b3b
Mitanshu Reshamwala <42906426+MitanshuShaBa@users.noreply.github.com>
2020-04-12 15:15:10 +0530

    Updates

---
+++ precache-manifest.01005cd0dfcad8c86d4bddd16473ffcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4023798a120a9840111ffb517d801d",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "19ab1f9a6cffacc1dcc3",
+    "url": "/CovidTracker/static/css/main.4d839316.chunk.css"
+  },
+  {
+    "revision": "b5fec344da6e52783790",
+    "url": "/CovidTracker/static/js/2.fafa074f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker/static/js/2.fafa074f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ab1f9a6cffacc1dcc3",
+    "url": "/CovidTracker/static/js/main.90444d93.chunk.js"
+  },
+  {
+    "revision": "f2c03383d0fc8c95ceda",
+    "url": "/CovidTracker/static/js/runtime-main.54929bbb.js"
+  }
+]);
